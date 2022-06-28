6f113154bb6c265d5b06738566809677ffa3ec7f
iankang <ianngech@gmail.com>
2022-06-28 23:17:50 +0300

    added list implementation

---
+++ first_principles/list_first_principle/ListNode.java
@@ -0,0 +1,32 @@
+
+package com.kangethe.datastructures;
+
+class ListNode<T> {
+
+  T data;
+  ListNode<T> nextNode;
+
+  ListNode(T object) {
+
+    this(object, null);
+
+  }
+
+  ListNode(T object, ListNode<T> node) {
+    
+    data = object;
+    nextNode = node;
+
+  } 
+
+  T getData() {
+
+    return data;
+  }
+
+  ListNode<T> getNext() {
+
+    return nextNode;
+  }
+
+}
